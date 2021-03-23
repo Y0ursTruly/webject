1fd72a945ef3f67f625d57f5774c363f6c3f0fa7
Y0ursTruly <60293767+Y0ursTruly@users.noreply.github.com>
2021-03-22 21:01:01 -0400

    Limitations fixed

--- serial.js
+++ serial.js
@@ -5,7 +5,7 @@ function objToString(obj,checkClone){ //concept from object cloner
   function mapToFile(m,files){var x=files
     if(!m||!m.length){return(files)}var errort=0
     m.forEach(a=>{if(x[a]){x=x[a]}else{errort=1}})
-    if(x==files||errort){return(0)}return(x)
+    if(errort){return(0)}return(x)
   }
   
   function casingOf(obj){if(obj instanceof Array){return[]}return{}}
@@ -14,6 +14,7 @@ function objToString(obj,checkClone){ //concept from object cloner
   
   function recurse(obj,newObj,map){
     Object.keys(obj).forEach((a,i)=>{
+      if(typeof obj[a]=="function"){return;} //skip keys that have their values as function
       const map1=[...map]; map.push(a)
       if(typeof obj[a]=="object"&&obj[a]!=null){
         newObj[a]=casingOf(obj[a])
@@ -41,8 +42,12 @@ function objToString(obj,checkClone){ //concept from object cloner
 }
 function stringToObj(string,obj){
   if(typeof obj!="object"){obj={}}
-  Object.keys(obj).forEach(a=>delete(obj[a])) //cleans object before rewriting
   var info=JSON.parse(string); let arr=[obj]
+  function mapToFile(m,files){var x=files
+    if(!m||!m.length){return(files)}var errort=0
+    m.forEach(a=>{if(x[a]){x=x[a]}else{errort=1}})
+    if(errort){return(0)}return(x)
+  }
   info.forEach(a=>{
     let x=obj; let index=a.path.length-1
     if(a.path.length==0){return obj.__proto__=a.value.__proto__}
@@ -50,8 +55,14 @@ function stringToObj(string,obj){
     if(Object.keys(a).includes("value")){x[a.path[index]]=a.value;arr.push(a.value)}
     else{x[a.path[index]]=arr[a.reference];arr.push(arr[a.reference])}
   })
+  //this part(the next 3 lines) for deleting ONLY the removed keys(instead of deleting everything and rewriting)
+  JSON.parse(objToString(obj))
+  .filter(a=>info.every(b=>JSON.stringify(b.path)!=JSON.stringify(a.path))) 
+  .forEach(a=>delete(mapToFile(a.path.slice(0,a.path.length-1),obj)[a.path[a.path.length-1]]))
+  //that line above is an eyefull, but the mapToFile returns the parent and the [] part is the name of the child
+  //that combination was made because cyclics proved more difficult to delete
   return obj
 }
 
 try{module.exports={objToString,stringToObj}}
-catch(err){console.log("Ah, you must be a browser")}
+catch(err){console.log("BTW, you must be a browser :D")}
