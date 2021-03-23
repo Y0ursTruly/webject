c8815739de4bdec6f9a92e3e31683052f13e1173
Y0ursTruly <60293767+Y0ursTruly@users.noreply.github.com>
2021-03-22 21:10:40 -0400

    new example
    functions are based a lot on context, therefore to efficiently clone functions, I would need to clone their contexts(which I cannot for the time being)

---
+++ Illustrations/functionSkipped.js
@@ -0,0 +1,19 @@
+let {serve,connect}=require('../webject.js')
+var mainObj={a:{a1:1,a2:{a3:0}},b:{b1:2},c:[3,4],f:function(){}} //key f gets skipped(because it's a function)
+mainObj.m=mainObj; //example object(it's cyclic too)
+
+(async()=>{
+  let myWebject=serve(mainObj) //I would let it make it's own server since I don't have one to give it to
+  /*if you do already have a server, you can just pass it in through the second argument*/
+  
+  setInterval(()=>mainObj.c[0]++,500) //slight edit to object every 500 ms
+  
+  let viewKey=myWebject.addToken(1) //a new view only token(because authLevel is 1)
+  
+  let serverLocation="ws://localhost:8009" //by default it would TRY to host on port 8009
+  
+  let sharedObj=await connect(serverLocation,viewKey)
+  setInterval(()=>{ //every 5 seconds show the shared cyclic object
+    console.log("\n\n\nThe main object is\n",mainObj); console.log("\n\nThe shared object is:\n",sharedObj)
+  },5000)
+})()
