99ca4159ae29496ae735c672b6c29562f824ff6e
Y0ursTruly <60293767+Y0ursTruly@users.noreply.github.com>
2021-04-13 20:57:24 -0400

    for one file to test almost everything

--- Illustrations/lockTesting.js
+++ Illustrations/lockTesting.js
@@ -1,3 +1,4 @@
+setTimeout(()=>process.exit(0),2001)
 let {serve,connect}=require('../webject.js')
 var mainObj={a:{a1:1,a2:{a3:0}},b:{b1:2},c:[3,4],undefined} //even undefined value gets shared >:D
 mainObj.m=mainObj; //example object(it's cyclic too)
