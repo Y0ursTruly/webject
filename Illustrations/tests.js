c0fce152099b3002c53ff90a17ed2ff2100156c1
Y0ursTruly <60293767+Y0ursTruly@users.noreply.github.com>
2021-04-13 20:57:27 -0400

    one file for running tests

---
+++ Illustrations/tests.js
@@ -0,0 +1,23 @@
+let {spawn}=require('child_process')
+let log=(text)=>console.log('\x1b[1m\x1b[33m'+text+'\x1b[0m')
+async function exec(command){
+  return await new Promise(resolve=>{
+    let options={stdio: 'inherit',env:process.env,cwd:undefined,shell:true}
+    let myChild=spawn(command,['build'],options)
+    myChild.on('close',resolve)
+  })
+}
+
+async function example(toLog,exampleName){
+  log('\n'+toLog); return await exec(`node "${__dirname}\\${exampleName}.js"`)
+};
+
+(async()=>{
+  await example("Sharing Object Test(first test)", "clientAndServer")
+  await example("Delete Test(even if cyclic deleted)", "deleteExample")
+  await example("Function Skip Test(functions not shared)", "functionSkipped")
+  await example("Multiple Objects Testing(one per authToken)", "multipleObjects")
+  await example("Lock Testing(an authToken can be locked)", "lockTesting")
+  await example("Events Testing(demonstration of events)", "eventsTesting")
+  await example("No Re-Pointing Test(nested objects are instead modified)", "noRePointing")
+})()
