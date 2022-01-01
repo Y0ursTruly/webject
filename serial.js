59a0e9e714847bb657eb8cb13c42e2ab279f1505
Y0ursTruly <60293767+Y0ursTruly@users.noreply.github.com>
2022-01-01 07:12:11 -0400

    Version 1.1.9

--- serial.js
+++ serial.js
@@ -1,13 +1,19 @@
-function objToString(obj,spacing,checkClone){ //concept from object cloner
+function mapToFile(m,files){var x=files
+  if(!m||!m.length){return(files)}var errort=0
+  m.forEach(a=>{if(x[a]){x=x[a]}else{errort=1}})
+  if(errort){return(0)}return(x)
+}
+let listFn=cache=>{
+  let toReturn=part=>{
+    part.path=JSON.stringify(part.path)
+    cache[part.path]=part
+  }
+  return toReturn
+}
+
+function objToString(obj,spacing,checkClone,cmpStr){ //concept from object cloner
   //put a truthy value for checkClone and it returns {clone,string}
   if(typeof obj!="object"){const x=obj; return x}
-  
-  function mapToFile(m,files){var x=files
-    if(!m||!m.length){return(files)}var errort=0
-    m.forEach(a=>{if(x[a]){x=x[a]}else{errort=1}})
-    if(errort){return(0)}return(x)
-  }
-  
   function casingOf(obj){if(obj instanceof Array){return[]}return{}}
   var newObj=casingOf(obj); var arr2=[{path:[],value:casingOf(obj)}]
   var arr=[obj]; var arr1=[[]]; var newObj1=newObj
@@ -38,21 +44,33 @@ function objToString(obj,spacing,checkClone){ //concept from object cloner
     })
   }
   recurse(obj,newObj,[]); var result=JSON.stringify(arr2,null,spacing||'')
-  if(checkClone){return{clone:newObj,string:result}}return result
+  if(checkClone&&!cmpStr){return{clone:newObj,string:result}}
+  if(!cmpStr){return result}
+  var finalResult=[], cache1={}, cache2={} //2 is old version, 1 is new version
+  let list1=JSON.parse(result), list2=JSON.parse(cmpStr)
+  list1.forEach(listFn(cache1)), list2.forEach(listFn(cache2))
+  Object.keys(cache2).forEach(key=>{if(!cache1[key]){
+    cache2[key].delete=true; finalResult.push(cache2[key])
+  }})
+  Object.keys(cache1).forEach(key=>{ let {stringify:str}=JSON
+    if(!cache2[key]){ finalResult.push(cache1[key]) }
+    else if(str(cache2[key])!=str(cache1[key])){ finalResult.push(cache1[key]) }
+  })
+  finalResult.forEach(part=>{ part.path=JSON.parse(part.path) })
+  if(checkClone){return{clone:newObj,string:JSON.stringify(finalResult)}}
+  return JSON.stringify(finalResult)
 }
-function stringToObj(string,obj){
+
+
+function stringToObj(string,obj,onlyDifference){
   if(typeof obj!="object"){obj={}}
   var info=JSON.parse(string); let arr=[obj]
-  function mapToFile(m,files){var x=files
-    if(!m||!m.length){return(files)}var errort=0
-    m.forEach(a=>{if(x[a]){x=x[a]}else{errort=1}})
-    if(errort){return(0)}return(x)
-  }
   info.forEach(a=>{
     let x=obj; let index=a.path.length-1
     if(a.path.length==0){return obj.__proto__=a.value.__proto__}
     a.path.slice(0,index).forEach(a=>x=x[a])
-    if(Object.keys(a).includes("value")){
+    if(a.delete){delete(x[a.path[index]])} //onlyDifference has to give delete indications like this
+    else if(Object.keys(a).includes("value")){
       if(typeof x[a.path[index]]=="object"&&typeof a.value=="object"){
         x[a.path[index]].__proto__=a.value.__proto__; a.value=x[a.path[index]]
       }
@@ -60,6 +78,7 @@ function stringToObj(string,obj){
     }
     else{x[a.path[index]]=arr[a.reference];arr.push(arr[a.reference])}
   })
+  if(onlyDifference){return obj} //only the difference was given and NOT the full object
   //this part(the next 3 lines) for deleting ONLY the removed keys(instead of deleting everything and rewriting)
   JSON.parse(objToString(obj))
   .filter(a=>info.every(b=>JSON.stringify(b.path)!=JSON.stringify(a.path))) 
@@ -68,6 +87,7 @@ function stringToObj(string,obj){
   //that combination was made because cyclics proved more difficult to delete
   return obj
 }
+
 
 try{module.exports={objToString,stringToObj}}
 catch(err){console.log("Part 2/2 loaded, LET'S GO >:D")}
